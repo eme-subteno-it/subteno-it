328c4a1969cb72bf73863a42a34725b843d2b7ad
Elodie Meunier <eme@subteno-it.com>
2020-06-10 11:29:01 +0200

    [ADD] Style design and texts

--- static/src/js/script.js
+++ static/src/js/script.js
@@ -1,10 +1,19 @@
 $(function(){
     
+    // Management scroll button
     $(".scroll").click(function() {
         var section = $("." + this.id);
         $("html,body").animate({scrollTop: section.offset().top}, 'slow'); 
     })
 
+    // Management button hamburger menu
+    var button = $('#btn-navigation');
+    var menu = $('#site-navigation');
+
+    button.click(function () {
+        menu.toggleClass('isOpen');
+        $('body').toggleClass('isOpen');
+    })
 
 });
 
